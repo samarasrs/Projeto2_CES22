aa29260d60050e36642ab36a9b34f68092735d8a
renatocraf <renato@renato_craf@yahoo.com.br>
2020-08-06 15:44:56 -0300

    inserindo_boards_static_templates

---
+++ boards/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Board
+
+# Create your views here.
+#from django.http import HttpResponse
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request,'home.html',{'boards':boards})
+
+def index(request):
+    return render(request,'index.html')
